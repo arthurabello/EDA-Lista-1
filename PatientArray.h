b5ebb88832b4cbe1f6f99109b3171d3905a7ac56
arthurabello <rabelolito@gmail.com>
2025-03-15 12:54:31 -0300

    docs: documenting constructed functions until the last commit and with a  b e t t e r documentation

--- PatientArray.h
+++ PatientArray.h
@@ -8,7 +8,7 @@ namespace PatientArrayTAD {
         int severity;         // Positive number. The higher the value, the more severe.
         char arrival_time[6]; // Format "XXhYY"
     };
-    
+
     struct PatientArray {
         Patient *patients; // Pointer to the array of patients
         int size;          // Current number of stored patients
@@ -17,29 +17,64 @@ namespace PatientArrayTAD {
 
     PatientArray *initializePatientArray();
 
-    /**
+        /**
+     * @brief Initializes an empty PatientArray structure.
+     * 
+     * Allocates memory for a dynamic array of patients with an initial capacity of 4.
+     * The structure starts empty (`size = 0`).
+     * 
+     * @return A pointer to the newly allocated PatientArray.
+     */
+
+    void printPatients(PatientArray *patient_array);
+
+        /**
      * @brief Displays the list of patients stored in the PatientArray structure.
      * 
      * Prints the capacity, current size, and the list of patients in the following format:
      * 
+     * ```
      * Capacity: <X>
      * Current size: <Y>
      * 
      * Patients:
      * * <Arrival_Time> | <Severity> | <Name>
      * * ...
+     * ```
      * 
      * If there are no patients, a message indicating that the list is empty is displayed.
      * 
-     * @param pa Pointer to the PatientArray structure to be displayed.
+     * @param patient_array Pointer to the PatientArray structure to be displayed.
      */
-    void printPatients(PatientArray *pa);
-    
-    void insertPatient(PatientArray *pa, Patient p);
+
+    void insertPatient(PatientArray *patient_array, Patient patient);
+
+        /**
+     * @brief Inserts a new patient at the end of the array.
+     * 
+     * If the array reaches 75% of its current capacity, it dynamically expands
+     * by doubling its size to accommodate more patients.
+     * 
+     * @param patient_array Pointer to the PatientArray structure where the patient will be inserted.
+     * @param patient The Patient struct containing the patient's information.
+     */
+
+    void destroyPatientArray(PatientArray* patient_array);
+
+        /**
+     * @brief Frees the memory allocated for a PatientArray structure.
+     * 
+     * - Deallocates the dynamic array of patients.
+     * - Deallocates the PatientArray structure itself.
+     * - Ensures no memory leaks occur.
+     * 
+     * @param patient_array Pointer to the PatientArray structure to be destroyed.
+     */
+
+
     int findNextPatient(PatientArray *pa);
     void removePatient(PatientArray *pa, int index);
     Patient popNextPatient(PatientArray *pa);
-    void destroyPatientArray(PatientArray* patient_array);
 
 }
 
